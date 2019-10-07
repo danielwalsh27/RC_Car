3fbbd5ff9e5acbfcdf75f1a2566fd66b5f6ea3c0
Daniel Walsh <dewalsh27@yahoo.com>
2019-10-06 21:22:56 -0700

    Update and rename Test_Ultrasonic to Test_Ultrasonic.py

---
+++ Test_Ultrasonic.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as gpio
+import time
+gpio.setmode(gpio.BOARD)
+gpio.setwarnings(False)
+
+gpio.setup(trig,gpio.OUT)
+gpio.setup(echo,gpio.IN)
+
+while(True):
+    if(count % 100 == 0):
+        time.sleep(10)
+    gpio.output(trig,False)
+    time.sleep(.05)
+    gpio.output(trig,True)
+    time.sleep(.05)
+    gpio.output(trig,False)
+
+    start = time.time()
+    end = time.time()
+    while (gpio.input(echo) == 0):
+        start = time.time()
+    while (gpio.input(echo) == 1):
+        end = time.time()
+    duration = end - start
+    distance = duration * 17150
+    distance = round(distance,2)
+    print ("Distance:", distance, "cm     ")
+
+  
+    
+   
